af4f567f4255c2a3830dbdab704cc91ccb65ada3
LuizHMAL <leite.luizhenrique94@gmail.com>
2025-07-06 21:21:32 -0300

    Ready for use

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css';
 import logoImage from './assets/W&Jlogo.png';
 import { Carrossel } from './Carrossel';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
