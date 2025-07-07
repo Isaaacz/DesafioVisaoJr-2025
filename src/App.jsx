c5ae6935a742996ae5eb29b8662591392a253628
LuizHMAL <leite.luizhenrique94@gmail.com>
2025-07-06 21:51:05 -0300

    esqueleto e carrossel prontos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import logoImage from './assets/w.png';
+import logoImage from './assets/W&Jlogo.png';
 import { Carrossel } from './Carrossel';
 
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
